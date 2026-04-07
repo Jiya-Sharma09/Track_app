277a794a30767276764e40f3d4ab64d1060a678f
Jiya-Sharma09 <jiyas4870@gmail.com>
2026-04-07 09:06:06 +0530

    adding color theme file

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _StateHomeScreen();
+}
+
+class _StateHomeScreen extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SingleChildScrollView(child: Column(
+      
+    )));
+  }
+}
---
+++ lib/ui_feature/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // TODO : replace the colors I choose later to add a pop of color on the top of primary theme
+  static const Color primary = Color(0xFF7F5AF0);
+  static const Color secondary = Color.fromARGB(255, 187, 235, 215);
+  static const Color accent = Color(0xFFFF8BA7);
+
+  // background : 
+  static const Color background = Color.fromARGB(253, 247, 222, 210);
+
+  // text heading 
+  static const Color heading = Color.fromARGB(252, 95, 31, 2);
+
+  //top left curve : 
+  static const Color topLeftCurve =  Color.fromARGB(194, 239, 135, 87);
+
+  // Neutral colors
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  static const Color grey100 = Color(0xFFF3F4F6);
+  static const Color grey300 = Color(0xFFD1D5DB);
+  static const Color grey500 = Color(0xFF6B7280);
+  static const Color grey800 = Color(0xFF1F2937);
+
+  // Error / status
+  static const Color error = Color(0xFFEF4444);
+}
