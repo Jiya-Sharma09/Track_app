3638efb6ca49b4691aa704abcdaa88fd64402813
Jiya-Sharma09 <jiyas4870@gmail.com>
2026-04-14 16:33:44 +0530

    adding getToDo function in homescreen and initState. API services and state variables updated

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -2,12 +2,11 @@ class Todo{
   final String id ; 
   final String title;
   final String description = "";
-  final DateTime dueDate;
   bool isDone;
  Todo({required this.id, 
- required this.title, required this.dueDate, required this.isDone});
+ required this.title, required this.isDone});
 
  factory Todo.fromJson(Map<String, dynamic>json){
-  return Todo(id: json['id'], title: json['title'], dueDate: DateTime.parse(json['dueDate']), isDone: json['Completed']);
+  return Todo(id: json['id'], title: json['title'], isDone: json['Completed']);
  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:horizontal_week_calendar/horizontal_week_calendar.dart';
+import 'package:track_app/model/todo_list_model.dart';
 import 'package:track_app/screens/login_screen.dart';
 import 'package:track_app/ui_feature/colors.dart';
+import 'package:track_app/service/api_service.dart';
+import 'package:track_app/model/todo_model.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,6 +12,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _StateHomeScreen extends State<HomeScreen> {
+  ApiService service = ApiService();
+  DateTime selectedDate = DateTime.now();
+  ToDoList? finalToDoList;
+  List<Todo> selectedDateTodo = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DateTime today = DateTime.now();
+    // api call for today !
+    getToDo(today);
+  }
+
+  void getToDo(DateTime dateHome) async {
+    try {
+      final _ToDoListTemp = await service.fetchTodoList(dateHome);
+      final _tasklistTemp = await service.fetchTodo(_ToDoListTemp.id);
+      setState(() {
+        finalToDoList = _ToDoListTemp;
+        selectedDateTodo = _tasklistTemp;
+      });
+    } catch (e) {
+      setState(() {
+        finalToDoList = null;
+        selectedDateTodo = [];
+      });
+      throw ScaffoldMessenger(child: SnackBar(content: Text("Error : $e")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +72,9 @@ class _StateHomeScreen extends State<HomeScreen> {
                         activeBackgroundColor: AppTheme.primaryDark1,
                         inactiveBackgroundColor: AppTheme.primaryLight1,
                         borderRadius: BorderRadius.circular(14),
+                        onDateChange: (value) {
+                          setState(() {});
+                        },
                       ),
                     ),
                   ),
@@ -69,9 +105,6 @@ class _StateHomeScreen extends State<HomeScreen> {
                 ),
 
                 // drawer screen !
-
-                  
-
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(
@@ -82,12 +115,10 @@ class _StateHomeScreen extends State<HomeScreen> {
                   child: Text("go back to login screen"),
                 ),
               ],
-
-              
             ),
           ),
 
-          // add draggable screen here !
+          // add draggable screen here :
         ],
       ),
     );
